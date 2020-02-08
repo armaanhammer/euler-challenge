b3e1fa01233a9543ce5bbfa26557e27834fdfafb
Armaan Roshani <armaan@gmail.com>
2020-02-07 17:11:56 -0800

    prettying

--- 2/2.go
+++ 2/2.go
@@ -4,12 +4,10 @@ import "fmt"
 
 func main() {
 	upperBound := 4000000
-	//fib := make([]int)
 	curr := 2
 	prev := 1
 	sum := 0
 
-	// set a for loop
 	for curr < upperBound {
 		if curr%2 == 0 {
 			sum += curr
--- 2/README.md
+++ 2/README.md
@@ -1,4 +1,4 @@
-Problem 2
+# Problem 2
 
 https://projecteuler.net/problem=2
 

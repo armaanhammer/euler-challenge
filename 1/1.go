85f4e7c7f76b58e6f128f3136fa3cc0e09fd25c4
Armaan Roshani <armaan@gmail.com>
2020-02-07 16:46:42 -0800

    Success!
    Fixed bug in problem 1, and answer was correct.

--- 1/1.go
+++ 1/1.go
@@ -8,12 +8,13 @@ func main() {
 	sum := 0
 
 	// set a for loop
-	for i := upperBound - 1; i > 0; i-- {
+	for i := 0; i < upperBound; i++ {
 		for j := range nums {
 			if i%nums[j] == 0 {
 				sum += i
+				fmt.Println("num:", i, "sum:", sum)
+				break // to prevent multiples being caught multiple times
 			}
-			//fmt.Println("num:", i, "sum:", sum)
 		}
 	}
 

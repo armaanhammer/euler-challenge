63fb544b336c98c7d813bacbfa92cf269381c46f
Armaan Roshani <armaan@gmail.com>
2020-02-20 22:55:51 -0800

    prettying

--- src/4/4.go
+++ src/4/4.go
@@ -22,13 +22,8 @@ func main() {
 		bigInt += 9
 	}
 
-	//fmt.Println(int1, int2)
-
 	var number, remainder, temp, final int
 	var reverse int = 0
-
-	//fmt.Print("Enter any positive integer : ")
-	//fmt.Scan(&number)
 
 	for i := bigInt; i > bigInt/10; i-- { //need better test condition
 		for j := bigInt; j > bigInt/10; j-- { //need better test condition
@@ -36,22 +31,15 @@ func main() {
 			temp = number
 			reverse = 0
 
-			//fmt.Println("made into first loop") //debug
-
 			for {
-				//fmt.Println("made into SECOND loop") //debug
 				remainder = number % 10
 				reverse = reverse*10 + remainder
 				number /= 10
 
 				if number == 0 {
-					//fmt.Println("temp:", temp, "\treverse:", reverse)
 					break // Break Statement used to exit from loop
 				}
 			}
-
-			//fmt.Println("temp:", temp)
-			//fmt.Println("reverse:", reverse)
 
 			if temp == reverse {
 				if final < temp {

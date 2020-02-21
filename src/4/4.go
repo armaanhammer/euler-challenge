55bb5c69747c8dbc8fd67e9a64b60ba946fb07da
Armaan Roshani <armaan@gmail.com>
2020-02-20 22:48:29 -0800

    prettying

--- src/4/4.go
+++ src/4/4.go
@@ -1,11 +1,16 @@
-// Golang program to check whether a number is palindrome or not
-// from: https://www.golangprograms.com/go-program-to-check-whether-a-number-is-palindrome-or-not.html
+// program for finding the largest palindrome made from the product of
+// two arbitrary-digit numbers. Program is tested against two 2-digit,
+// 3-digit, and 4-digit numbers. Attempted testing against 5-digit numbers,
+// but killed program after it took more than 30 seconds. Clearly it could
+// be better optimized.
+//
+// Armaan Roshani
 
 package main
 
 import "fmt"
 
-var digits int = 4 // size of ints to multiply
+var digits int = 3 // size of ints to multiply
 
 func main() {
 	bigInt := 0

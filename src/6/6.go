07cca05d8234fbf404014520cd32bc01565497d6
Armaan Roshani <armaan@gmail.com>
2020-02-29 19:22:29 -0800

    working on problem 6

---
+++ src/6/6.go
@@ -0,0 +1,24 @@
+// Program for finding the difference between the sum of the squares of
+// the first one hundred natural numbers and the square of the sum.
+//
+// Armaan Roshani
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	num := 10 // number of naturals to process
+	sumOfSquares := 0
+	squareOfSum := 0
+
+	for i = 0; i < num; i++ {
+
+	}
+
+	//fmt.Printf("%d is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20\n", initVal)
+
+}
